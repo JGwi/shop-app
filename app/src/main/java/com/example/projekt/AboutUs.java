958ff59fc52a055d121a04ae3e4ddac9f48ed475
JGwi <gwizdzjakub2@gmail.com>
2022-11-02 22:05:19 +0100

    Added a comment :)

--- app/src/main/java/com/example/projekt/AboutUs.java
+++ app/src/main/java/com/example/projekt/AboutUs.java
@@ -16,6 +16,7 @@ public class AboutUs extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
     }
+    //hgdsagdjasgdjhgdahdgajgs
 
     public void ClickMenu(View view){
         MainActivity.openDrawer(drawerLayout);
